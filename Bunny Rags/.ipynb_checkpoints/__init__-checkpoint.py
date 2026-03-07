b5b4074d1d86fbea63db90ea17e0551de672452a
mlgoering <mlgoering@gmail.com>
2026-03-07 10:26:07 +0100

    chore: stop tracking notebook checkpoint artifacts

--- Bunny Rags/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from .builder import CausalGraphBuilder
-from .bunny_retriever import BunnyPathRetriever
-from .explainer import CausalGraphExplainer
-
-__all__ = [
-    'CausalGraphBuilder',
-    'BunnyPathRetriever',
-    'CausalGraphExplainer'
-] 
